e600b1ec2157cd8c86916cb0c92a235144c91c61
yhf <youssifhany75@gmail.com>
2022-11-28 01:26:42 +0200

    add: esbuild service and use plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+
 import './App.css';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 function App() {
-  return <div className="App"></div>;
+  const [initialized, setInitialized] = useState(false);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    if (initialized) return;
+    try {
+      await esbuild.initialize({
+        worker: true,
+        wasmURL: 'https://www.unpkg.com/esbuild-wasm@0.15.16/esbuild.wasm',
+      });
+
+      setInitialized(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    startService();
+  });
+
+  const clickHandler = async () => {
+    if (!initialized) return;
+
+    // To escape from vite replacement of process.env.NODE_ENV
+    const envKey = ['process', 'env', 'NODE_ENV'].join('.');
+
+    const { outputFiles } = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        [envKey]: '"production"',
+        global: 'window',
+      },
+    });
+
+    // console.log(outputFiles[0].text);
+
+    setCode(outputFiles[0].text);
+  };
+
+  return (
+    <div className="App">
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}>
+        Hello
+      </textarea>
+      <div>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
