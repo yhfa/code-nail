2e94518d167a45c1252291fca002d8301cc28167
yhf <youssifhany75@gmail.com>
2022-12-14 23:16:23 +0200

    add: action bar

---
+++ src/components/actionBar.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import { useAction } from '../hooks/useAction';
+
+interface IActionBar {
+  id: string;
+}
+
+const ActionBar: FC<IActionBar> = ({ id }) => {
+  const { moveCell, deleteCell } = useAction();
+  return (
+    <div className="group absolute top-0 right-0 z-10">
+      <div className="opacity-25 transition-opacity duration-300 group-hover:opacity-100">
+        <button
+          className="bg-orange-500 py-1 px-2 text-white"
+          onClick={() => moveCell(id, 'up')}
+        >
+          <span>
+            <i className="fas fa-arrow-up"></i>
+          </span>
+        </button>
+        <button
+          className="bg-orange-500 py-1 px-2 text-white"
+          onClick={() => moveCell(id, 'down')}
+        >
+          <span>
+            <i className="fas fa-arrow-down"></i>
+          </span>
+        </button>
+        <button
+          className="bg-orange-500 py-1 px-2 text-white"
+          onClick={() => deleteCell(id)}
+        >
+          <span className="icon">
+            <i className="fas fa-times"></i>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ActionBar;
