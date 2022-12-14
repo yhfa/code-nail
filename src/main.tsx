b499c535c9b977007dad9bdc3ff0f8d68f39d839
yhf <youssifhany75@gmail.com>
2022-12-14 23:16:02 +0200

    add: fontawesome to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { store } from './redux';
 import App from './App';
