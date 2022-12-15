c53369bbf84e9a09afacecb41ec5390a1f4a8744
yhf <youssifhany75@gmail.com>
2022-12-15 18:20:24 +0200

    update: memoize the action hook

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -1,9 +1,12 @@
+import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../redux';
 
 export const useAction = () => {
   const dispatch = useDispatch();
-
-  return bindActionCreators(actionCreators, dispatch);
+  return useMemo(
+    () => bindActionCreators(actionCreators, dispatch),
+    [dispatch]
+  );
 };
