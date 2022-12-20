a5848506a688ba773c84e279a66d652c5a9bd929
yhf <youssifhany75@gmail.com>
2022-12-20 17:02:35 +0200

    add: serve command logic

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,11 +2,41 @@ import path from "path";
 import { Command } from "commander";
 import { serve } from "local-api";
 
+interface ILocalApiError {
+  code: string;
+}
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(Number(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(
+        Number(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+
+      console.log(`
+      Opened ${filename} file. Navigate tp http://localhost:${options.port} to edit the file.
+      `);
+    } catch (error) {
+      const isLocalApiError = (error: any): error is ILocalApiError => {
+        return typeof error.code === "string";
+      };
+
+      if (isLocalApiError(error)) {
+        if (error.code === "EADDRINUSE") {
+          console.error("Port is in use. Try running on a different port.");
+        }
+      } else if (error instanceof Error) {
+        console.log("Heres the problem", error.message);
+      }
+      process.exit(1);
+    }
   });
